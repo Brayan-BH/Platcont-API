f67a6247ac1e3f559b466b4a54183b96f7a3baef
Deybin Yoni Gil Perez <deybin_04@hotmail.com>
2023-02-03 13:08:37 -0500

    Upload Files

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter().StrictSlash(true)
+	r.HandleFunc("/", HomeHandler)
+	//Rutas de autentificacion
+	fmt.Println("Server on port 5000")
+	log.Fatal(http.ListenAndServe(":5000", r))
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "aplicattion/json")
+	data := map[string]interface{}{"api": "platcontApi", "version": 1.1}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(data)
+}
